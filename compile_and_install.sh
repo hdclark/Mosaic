fffdf6388c0adeb36e7ea4fe2a3804e71ad75bb7
Hal <haley.clark@bccancer.bc.ca>
2017-09-27 12:09:46 -0700

    Incomplete: some extras.

---
+++ compile_and_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+BUILDDIR="/home/hal/Builds/Mosaic/"
+BUILTPKGSDIR="/home/hal/Builds/"
+
+mkdir -p "${BUILDDIR}"
+#rsync -avz --cvs-exclude --delete ./ "${BUILDDIR}"  # Removes CMake cache files, forcing a fresh rebuild.
+rsync -avz --cvs-exclude ./ "${BUILDDIR}"
+
+pushd .
+cd "${BUILDDIR}"
+makepkg --syncdeps --install --noconfirm
+mv "${BUILDDIR}/"*.pkg.tar.* "${BUILTPKGSDIR}/"
+popd
+
